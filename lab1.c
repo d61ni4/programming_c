5e31fbfbcca0177e73e8bd33347031de4b4ee69a
d61ni4 <118959145+d61ni4@users.noreply.github.com>
2024-02-10 15:49:33 +1000

    added different cases

--- lab1.c
+++ lab1.c
@@ -2,8 +2,17 @@
 #include <math.h>
 
 int main() {
-  double a, b, c;
+  double a, b, c, D;
   scanf("%lf %lf %lf", &a, &b, &c);
-  printf("%lf, %lf", (-b + sqrt(b*b - 4*a*c))/(2*a), (-b - sqrt(b*b - 4*a*c))/(2*a));
+  D = b*b - 4*a*c;
+  
+  if (D > 0) {
+    printf("two roots: %lf, %lf", (-b + sqrt(D))/(2*a), (-b - sqrt(D))/(2*a));
+  } else if (D == 0) {
+    printf("one root: %lf", -b/(2*a));
+  } else {
+    printf("no solution");
+  }
+
   return 0;
 }
